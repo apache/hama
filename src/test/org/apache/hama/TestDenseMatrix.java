2dcb2ac6b30ebf0d6b04f6f6a1f07908a87c2930
Edward J. Yoon <edwardyoon@apache.org>
2009-06-10 08:48:41 +0000

    Add matrix subtraction test case
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama/trunk@783255 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/hama/TestDenseMatrix.java
+++ src/test/org/apache/hama/TestDenseMatrix.java
@@ -178,6 +178,15 @@ public class TestDenseMatrix extends TestCase {
         assertEquals(result.get(i, j), m1.get(i, j) + m2.get(i, j));
       }
     }
+
+    Matrix subtract = result.add(-1.0, m2);
+
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        double gap = (subtract.get(i, j) - m1.get(i, j));
+        assertTrue(-0.00001 < gap && gap < 0.00001);
+      }
+    }
   }
 
   public void testMultiMatrixAdd() throws IOException {
