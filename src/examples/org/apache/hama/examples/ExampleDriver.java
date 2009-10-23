2127cea147cc333fdc87753c4c1f2d5e840656a8
Edward J. Yoon <edwardyoon@apache.org>
2009-10-23 02:03:37 +0000

    cosine similarity matrix
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama/trunk@828916 13f79535-47bb-0310-9956-ffa450edef68

--- src/examples/org/apache/hama/examples/ExampleDriver.java
+++ src/examples/org/apache/hama/examples/ExampleDriver.java
@@ -29,7 +29,7 @@ public class ExampleDriver {
       pgd.addClass("rand", RandomMatrix.class, "Generate matrix with random elements.");
       pgd.addClass("add", MatrixAddition.class, "Mat-Mat addition.");
       pgd.addClass("mult", MatrixMultiplication.class, "Mat-Mat multiplication.");
-      pgd.addClass("multfiles", FileMatrixBlockMult.class, "file matrices multiplication.");
+      pgd.addClass("similarity", CosineSimilarityMatrix.class, "Cosine Similarity Matrix.");
       pgd.addClass("norm", MatrixNorm.class, "Maximum absolute row sum of matrix");
       pgd.driver(args);
     } catch (Throwable e) {
---
+++ src/test/org/apache/hama/examples/TestCosineSimilarityMatrix.java
@@ -0,0 +1,114 @@
+package org.apache.hama.examples;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.IdentityTableReducer;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hama.Constants;
+import org.apache.hama.HamaCluster;
+import org.apache.hama.HamaConfiguration;
+import org.apache.hama.matrix.DenseMatrix;
+import org.apache.hama.matrix.DenseVector;
+import org.apache.hama.matrix.Matrix;
+import org.apache.hama.matrix.Vector;
+import org.apache.hama.util.BytesUtil;
+import org.apache.log4j.Logger;
+
+public class TestCosineSimilarityMatrix extends HamaCluster {
+  static final Logger LOG = Logger.getLogger(TestCosineSimilarityMatrix.class);
+  private int SIZE = 10;
+  private Matrix m1;
+  private Matrix symmetricMatrix;
+  private HamaConfiguration conf;
+
+  /**
+   * @throws UnsupportedEncodingException
+   */
+  public TestCosineSimilarityMatrix() throws UnsupportedEncodingException {
+    super();
+  }
+
+  public void setUp() throws Exception {
+    super.setUp();
+
+    conf = getConf();
+
+    m1 = DenseMatrix.random(conf, SIZE, SIZE);
+    symmetricMatrix = new DenseMatrix(conf, SIZE, SIZE);
+  }
+
+  public void testCosineSimilarity() throws IOException {
+    Job job = new Job(conf, "set MR job test");
+    job.getConfiguration().set("input.matrix", m1.getPath());
+
+    Scan scan = new Scan();
+    scan.addFamily(Bytes.toBytes(Constants.COLUMN_FAMILY));
+
+    TableMapReduceUtil.initTableMapperJob(m1.getPath(), scan,
+        ComputeSimilarity.class, ImmutableBytesWritable.class, Put.class, job);
+    TableMapReduceUtil.initTableReducerJob(symmetricMatrix.getPath(),
+        IdentityTableReducer.class, job);
+    job.setNumReduceTasks(0);
+    try {
+      job.waitForCompletion(true);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    Vector v1 = m1.getRow(0);
+    Vector v2 = m1.getRow(2);
+    assertEquals(v1.dot(v2), symmetricMatrix.get(0, 2));
+  }
+
+  public static class ComputeSimilarity extends
+      TableMapper<ImmutableBytesWritable, Put> implements Configurable {
+    private Configuration conf = null;
+    private DenseMatrix matrix;
+
+    public void map(ImmutableBytesWritable key, Result value, Context context)
+        throws IOException, InterruptedException {
+      DenseVector v = new DenseVector(value);
+
+      Put put = new Put(key.get());
+      for (int i = 0; i < matrix.getRows(); i++) {
+        double dotProduct = matrix.getRow(i).dot(v);
+        if (BytesUtil.getRowIndex(key.get()) == i) {
+          dotProduct = 0;
+        }
+        put.add(Bytes.toBytes(Constants.COLUMN_FAMILY), Bytes.toBytes(String
+            .valueOf(i)), BytesUtil.doubleToBytes(dotProduct));
+      }
+
+      context.write(key, put);
+    }
+
+    @Override
+    public Configuration getConf() {
+      return conf;
+    }
+
+    @Override
+    public void setConf(Configuration conf) {
+      this.conf = conf;
+      try {
+        matrix = new DenseMatrix(new HamaConfiguration(), conf
+            .get("input.matrix"));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+    }
+  }
+}
