97b4fbe9a6e1153392458334fc32a76dbd10a04f
Edward J. Yoon <edwardyoon@apache.org>
2009-11-09 08:46:21 +0000

    Add simple admin tool for manage the temporary tables.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama/trunk@833996 13f79535-47bb-0310-9956-ffa450edef68

--- conf/hama-env.sh
+++ conf/hama-env.sh
@@ -22,19 +22,19 @@
 # Set environment variables here.
 
 # The java implementation to use.  Required.
-# export JAVA_HOME=/usr/lib/j2sdk1.5-sun
+export JAVA_HOME=/usr/java/jdk1.6.0_10
 
 # hadoop conf dir. to find the mapreduce cluster.
-# export HADOOP_CONF_DIR=
+export HADOOP_CONF_DIR=/usr/local/src/hadoop-0.20.1/conf
 
 # hbase conf dir. to find the hbase cluster.
-# export HBASE_CONF_DIR= 
+export HBASE_CONF_DIR=/usr/local/src/hbase-0.20.0/conf
 
 # Extra Java CLASSPATH elements.  Optional.
-# export HAMA_CLASSPATH=$HADOOP_CONF_DIR:$HBASE_CONF_DIR
+export HAMA_CLASSPATH=$HADOOP_CONF_DIR:$HBASE_CONF_DIR
 
 # The maximum amount of heap to use, in MB. Default is 1000.
-# export HBASE_HEAPSIZE=1000
+export HBASE_HEAPSIZE=1000
 
 # Where log files are stored.  $HAMA_HOME/logs by default.
 # export HAMA_LOG_DIR=${HAMA_HOME}/logs
