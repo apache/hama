2c8c0e90484e5a6a1a19f747e692cc6cd0cfd549
Edward J. Yoon <edwardyoon@apache.org>
2009-11-11 06:13:11 +0000

    Add benchmarking tool of eigenvalue operation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama/trunk@834777 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/hama/Benchmarks.java
@@ -0,0 +1,24 @@
+package org.apache.hama;
+
+import org.apache.hama.matrix.DenseMatrix;
+
+public class Benchmarks {
+
+  public static void main(String[] args) throws Exception {
+    if (args.length < 2) {
+      System.out.println("Usage: <size> <max-iterations>");
+      System.exit(-1);
+    }
+
+    HamaConfiguration conf = new HamaConfiguration();
+    System.out.println("Creating random matrix");
+    DenseMatrix rand = DenseMatrix.random_mapred(conf, Integer
+        .parseInt(args[0]), Integer.parseInt(args[0]));
+
+    double start = System.currentTimeMillis();
+    rand.jacobiEigenValue(Integer.parseInt(args[1]));
+    double end = System.currentTimeMillis();
+    System.out.println("Runtime: " + ((end - start)) * 1000 + " sec");
+  }
+
+}
