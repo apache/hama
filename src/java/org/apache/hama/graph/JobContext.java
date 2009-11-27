5e168fe524f54f056067b5a4cc1ecf98c1da3389
Edward J. Yoon <edwardyoon@apache.org>
2009-11-27 01:01:54 +0000

    initial codebase for angrapa
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama/trunk@884732 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/hama/graph/JobContext.java
@@ -0,0 +1,111 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hama.graph;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.RawComparator;
+import org.apache.hadoop.io.Text;
+
+/**
+ * A read-only view of the job that is provided to the tasks while they are
+ * running.
+ */
+public class JobContext {
+  // Put all of the attribute names in here so that Job and JobContext are
+  // consistent.
+  protected static final String INPUT_FORMAT_CLASS_ATTR = "angrapa.inputformat.class";
+  protected static final String WALKER_CLASS_ATTR = "angrapa.walker.class";
+  protected static final String OUTPUT_FORMAT_CLASS_ATTR = "angrapa.outputformat.class";
+
+  protected final Configuration conf;
+  private final JobID jobId;
+
+  public JobContext(Configuration conf, JobID jobId) {
+    this.conf = conf;
+    this.jobId = jobId;
+  }
+
+  public Configuration getConfiguration() {
+    return conf;
+  }
+
+  public JobID getJobID() {
+    return jobId;
+  }
+
+  public Path getWorkingDirectory() throws IOException {
+    String name = conf.get("angrapa.working.dir");
+
+    if (name != null) {
+      return new Path(name);
+    } else {
+      try {
+        Path dir = FileSystem.get(conf).getWorkingDirectory();
+        conf.set("angrapa.working.dir", dir.toString());
+        return dir;
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  public Class<?> getOutputKeyClass() {
+    return conf.getClass("angrapa.output.key.class", LongWritable.class,
+        Object.class);
+  }
+
+  public Class<?> getOutputValueClass() {
+    return conf
+        .getClass("angrapa.output.value.class", Text.class, Object.class);
+  }
+
+  public String getJobName() {
+    return conf.get("angrapa.job.name", "");
+  }
+
+  @SuppressWarnings("unchecked")
+  public Class<? extends InputFormat<?, ?>> getInputFormatClass()
+      throws ClassNotFoundException {
+    return (Class<? extends InputFormat<?, ?>>) conf.getClass(
+        INPUT_FORMAT_CLASS_ATTR, InputFormat.class); // TODO: To be corrected
+    // to an implemented class
+  }
+
+  @SuppressWarnings("unchecked")
+  public Class<? extends OutputFormat<?, ?>> getOutputFormatClass()
+      throws ClassNotFoundException {
+    return (Class<? extends OutputFormat<?, ?>>) conf.getClass(
+        OUTPUT_FORMAT_CLASS_ATTR, OutputFormat.class); // TODO: To be corrected
+    // to an implemented
+    // class
+  }
+
+  public RawComparator<?> getSortComparator() {
+    return null;
+  }
+
+  public String getJar() {
+    return conf.get("walker.jar");
+  }
+}
---
+++ src/java/org/apache/hama/graph/OutputCommitter.java
@@ -0,0 +1,84 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hama.graph;
+
+import java.io.IOException;
+
+/**
+ * 
+ * @see JobContext
+ * @see TaskAttemptContext
+ * 
+ */
+public abstract class OutputCommitter {
+  /**
+   * For the framework to setup the job output during initialization
+   * 
+   * @param jobContext Context of the job whose output is being written.
+   * @throws IOException if temporary output could not be created
+   */
+  public abstract void setupJob(JobContext jobContext) throws IOException;
+
+  /**
+   * For cleaning up the job's output after job completion
+   * 
+   * @param jobContext Context of the job whose output is being written.
+   * @throws IOException
+   */
+  public abstract void cleanupJob(JobContext jobContext) throws IOException;
+
+  /**
+   * Sets up output for the task.
+   * 
+   * @param taskContext Context of the task whose output is being written.
+   * @throws IOException
+   */
+  public abstract void setupTask(TaskAttemptContext taskContext)
+      throws IOException;
+
+  /**
+   * Check whether task needs a commit
+   * 
+   * @param taskContext
+   * @return true/false
+   * @throws IOException
+   */
+  public abstract boolean needsTaskCommit(TaskAttemptContext taskContext)
+      throws IOException;
+
+  /**
+   * To promote the task's temporary output to final output location
+   * 
+   * The task's output is moved to the job's output directory.
+   * 
+   * @param taskContext Context of the task whose output is being written.
+   * @throws IOException if commit is not
+   */
+  public abstract void commitTask(TaskAttemptContext taskContext)
+      throws IOException;
+
+  /**
+   * Discard the task output
+   * 
+   * @param taskContext
+   * @throws IOException
+   */
+  public abstract void abortTask(TaskAttemptContext taskContext)
+      throws IOException;
+}
