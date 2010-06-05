4dd480d6014a408a60a6d2f8363cba024549affb
Hyunsik Choi <hyunsik@apache.org>
2010-06-05 07:45:28 +0000

    HAMA-270 : wrong sequence of readFields() of ClusterStatus
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama/trunk@951667 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/hama/bsp/TestClusterStatus.java
@@ -0,0 +1,56 @@
+package org.apache.hama.bsp;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import org.apache.hadoop.io.DataInputBuffer;
+import org.apache.hadoop.io.DataOutputBuffer;
+
+import junit.framework.TestCase;
+
+public class TestClusterStatus extends TestCase {
+  Random rnd = new Random();
+  
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  public final void testWriteAndReadFields() throws IOException {
+    DataOutputBuffer out = new DataOutputBuffer();
+    DataInputBuffer in = new DataInputBuffer();
+    
+    ClusterStatus status1;
+    List<String> grooms = new ArrayList<String>();
+        
+    for(int i=0;i< 10;i++) {
+      grooms.add("groom_"+rnd.nextInt());
+    }
+    
+    int tasks = rnd.nextInt(100);
+    int maxTasks = rnd.nextInt(100);
+    BSPMaster.State state = BSPMaster.State.RUNNING;
+    
+    status1 = new ClusterStatus(grooms, tasks, maxTasks, state);    
+    status1.write(out);
+    
+    in.reset(out.getData(), out.getLength());
+   
+    ClusterStatus status2 = new ClusterStatus();
+    status2.readFields(in);
+    
+    Set<String> grooms_s = new HashSet<String>(status1.getActiveGroomNames());
+    Set<String> grooms_o = new HashSet<String>(status2.getActiveGroomNames());     
+    
+    assertEquals(status1.getGroomServers(), status2.getGroomServers());
+    
+    assertTrue(grooms_s.containsAll(grooms_o));
+    assertTrue(grooms_o.containsAll(grooms_s));    
+    
+    assertEquals(status1.getTasks(),status2.getTasks());
+    assertEquals(status1.getMaxTasks(), status2.getMaxTasks());
+  }
+}
