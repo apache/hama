ebc38d4ceb109b60292576ca520b946d4b09c7e2
Edward J. Yoon <edwardyoon@apache.org>
2011-11-16 08:24:31 +0000

    Minor refactor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/hama/trunk@1202580 13f79535-47bb-0310-9956-ffa450edef68

--- graph/src/main/java/org/apache/hama/graph/VertexArrayWritable.java
+++ graph/src/main/java/org/apache/hama/graph/VertexArrayWritable.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hama.examples.graph;
+package org.apache.hama.graph;
 
 import org.apache.hadoop.io.ArrayWritable;
 
