6b8096c59df385a067103a801be41b117839a5cc
Tommaso Teofili <tommaso@apache.org>
2012-11-03 13:47:07 +0000

    [HAMA-666] - improving unit test for linear regression
    git-svn-id: https://svn.apache.org/repos/asf/hama/trunk@1405333 13f79535-47bb-0310-9956-ffa450edef68

--- ml/src/test/java/org/apache/hama/ml/regression/LinearRegressionModelTest.java
+++ ml/src/test/java/org/apache/hama/ml/regression/LinearRegressionModelTest.java
@@ -12,12 +12,20 @@ import static org.junit.Assert.assertEquals;
 public class LinearRegressionModelTest {
 
   @Test
-  public void testCorrectCalculation() throws Exception {
+  public void testCorrectCostCalculation() throws Exception {
     LinearRegressionModel linearRegressionModel = new LinearRegressionModel();
     DoubleVector x = new DenseDoubleVector(new double[]{2, 3, 4});
     double y = 1;
     DoubleVector theta = new DenseDoubleVector(new double[]{1, 1, 1});
     Double cost = linearRegressionModel.calculateCostForItem(x, y, 2, theta);
     assertEquals("wrong cost calculation for linear regression", Double.valueOf(16d), cost);
+  }
+
+  @Test
+  public void testCorrectHypothesisCalculation() throws Exception {
+    LinearRegressionModel linearRegressionModel = new LinearRegressionModel();
+    Double hypothesisValue = linearRegressionModel.applyHypothesis(new DenseDoubleVector(new double[]{1, 1, 1}),
+            new DenseDoubleVector(new double[]{2, 3, 4}));
+    assertEquals("wrong hypothesis value for linear regression", Double.valueOf(9), hypothesisValue);
   }
 }
