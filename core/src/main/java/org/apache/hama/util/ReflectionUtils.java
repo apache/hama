5a648dab6c4c90bfc5cc8b945e8815c024b33e0d
Edward J. Yoon <edwardyoon@apache.org>
2015-05-21 03:21:36 +0000

    HAMA-957: Compilation failure with hadoop 2.7.0
    git-svn-id: https://svn.apache.org/repos/asf/hama/trunk@1680725 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/hama/util/ReflectionUtils.java
+++ core/src/main/java/org/apache/hama/util/ReflectionUtils.java
@@ -17,6 +17,11 @@
  */
 package org.apache.hama.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
 import java.lang.reflect.Constructor;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -128,4 +133,83 @@ public class ReflectionUtils {
     return result;
   }
 
+  static private ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
+
+  /**
+   * Print all of the thread's information and stack traces.
+   * 
+   * @param stream the stream to
+   * @param title a string title for the stack trace
+   */
+  public synchronized static void printThreadInfo(PrintWriter stream,
+      String title) {
+    final int STACK_DEPTH = 20;
+    boolean contention = threadBean.isThreadContentionMonitoringEnabled();
+    long[] threadIds = threadBean.getAllThreadIds();
+    stream.println("Process Thread Dump: " + title);
+    stream.println(threadIds.length + " active threads");
+    for (long tid : threadIds) {
+      ThreadInfo info = threadBean.getThreadInfo(tid, STACK_DEPTH);
+      if (info == null) {
+        stream.println("  Inactive");
+        continue;
+      }
+      stream.println("Thread "
+          + getTaskName(info.getThreadId(), info.getThreadName()) + ":");
+      Thread.State state = info.getThreadState();
+      stream.println("  State: " + state);
+      stream.println("  Blocked count: " + info.getBlockedCount());
+      stream.println("  Waited count: " + info.getWaitedCount());
+      if (contention) {
+        stream.println("  Blocked time: " + info.getBlockedTime());
+        stream.println("  Waited time: " + info.getWaitedTime());
+      }
+      if (state == Thread.State.WAITING) {
+        stream.println("  Waiting on " + info.getLockName());
+      } else if (state == Thread.State.BLOCKED) {
+        stream.println("  Blocked on " + info.getLockName());
+        stream.println("  Blocked by "
+            + getTaskName(info.getLockOwnerId(), info.getLockOwnerName()));
+      }
+      stream.println("  Stack:");
+      for (StackTraceElement frame : info.getStackTrace()) {
+        stream.println("    " + frame.toString());
+      }
+    }
+    stream.flush();
+  }
+
+  private static long previousLogTime = 0;
+
+  /**
+   * Log the current thread stacks at INFO level.
+   * 
+   * @param log the logger that logs the stack trace
+   * @param title a descriptive title for the call stacks
+   * @param minInterval the minimum time from the last
+   */
+  public static void logThreadInfo(Log log, String title, long minInterval) {
+    boolean dumpStack = false;
+    if (log.isInfoEnabled()) {
+      synchronized (ReflectionUtils.class) {
+        long now = System.currentTimeMillis();
+        if (now - previousLogTime >= minInterval * 1000) {
+          previousLogTime = now;
+          dumpStack = true;
+        }
+      }
+      if (dumpStack) {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        printThreadInfo(new PrintWriter(buffer), title);
+        log.info(buffer.toString());
+      }
+    }
+  }
+
+  private static String getTaskName(long id, String name) {
+    if (name == null) {
+      return Long.toString(id);
+    }
+    return id + " (" + name + ")";
+  }
 }
